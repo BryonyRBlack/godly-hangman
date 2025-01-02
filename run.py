dc84d2189c28bcab48b14436f79ec82be998db3a
Bryony Black <ikow8b1w@students.codeinstitute.net>
2025-01-02 12:17:55 +0000

    Add code to import the word lists, and the Hangman images

--- run.py
+++ run.py
@@ -2,6 +2,16 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+
+
+#Word lists to import
+from greek import greek_gods
+from roman import roman_gods
+from viking import viking_gods
+
+#The Hangman image to be printed after each guess
+from hangman import HANGMAN
+
 #This function selects a random word from the list for the user to try and guess
 def word_to_guess():
     word = random.choice(greek_gods)
